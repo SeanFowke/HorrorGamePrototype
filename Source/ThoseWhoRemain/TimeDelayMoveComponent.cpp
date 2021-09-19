e38c9da08a222486eef8d3cd957cdc805f5f6093
SeanFowke <seanfowke@hotmail.com>
2021-09-19 02:27:40 -0500

    Updates made to map

---
+++ Source/ThoseWhoRemain/TimeDelayMoveComponent.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeDelayMoveComponent.h"
+#include "Components/AudioComponent.h"
+
+// Sets default values for this component's properties
+UTimeDelayMoveComponent::UTimeDelayMoveComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+	audioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTimeDelayMoveComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(timerHandle, this, &UTimeDelayMoveComponent::MoveAfterDelay, delay, false);
+	audioComp->Stop();
+	// ...
+	
+}
+
+
+void UTimeDelayMoveComponent::MoveAfterDelay()
+{
+	shouldMove = true;
+
+	if (audioComp)
+	{
+		audioComp->Play();
+	}
+}
+
+void UTimeDelayMoveComponent::HandleMovement()
+{
+	if (GetOwner())
+	{
+		FVector currentLoc = GetOwner()->GetActorLocation();
+		FVector dir = moveLoc->GetActorLocation() - currentLoc;
+		dir.Normalize();
+		GetOwner()->SetActorLocation(currentLoc + (dir * movementSpeed));
+
+		if (FVector::Dist(moveLoc->GetActorLocation(), currentLoc) <= 5.0f)
+		{
+			shouldMove = false;
+		}
+	}
+}
+
+// Called every frame
+void UTimeDelayMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (shouldMove)
+	{
+		HandleMovement();
+	}
+	// ...
+}
+
---
+++ Source/ThoseWhoRemain/TimeDelayMoveComponent.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TimeDelayMoveComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class THOSEWHOREMAIN_API UTimeDelayMoveComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTimeDelayMoveComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// movement of the floor
+	UPROPERTY(EditAnywhere)
+	float delay;
+
+	UPROPERTY(EditAnywhere)
+	AActor* moveLoc;
+
+	bool shouldMove = false;
+
+	UPROPERTY(EditAnywhere)
+	float movementSpeed;
+	// sounds
+	UPROPERTY(EditAnywhere)
+	class UAudioComponent* audioComp;
+
+	// functions
+	void MoveAfterDelay();
+	void HandleMovement();
+	// timer
+	FTimerHandle timerHandle;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
